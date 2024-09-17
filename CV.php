a0276498ed2fb7fa4a499c1e5c47db836bef1f19
mangjayy <komangjay.14@gmail.com>
2024-09-17 19:45:22 +0800

    CV
    CV

---
+++ CV.php
@@ -0,0 +1,17 @@
+<?php
+
+$jenis_kelamin = 'L';
+$nama_lengkap = "Gusti Komang Sukma Wijaya";
+$umur = 21;
+$berat = 61.;
+$tinggi = 173.0;
+$menikah = false;
+
+echo '<br><img src="latar merah.png"></br>';
+echo "nama          : $nama_lengkap<br>";
+echo "jenis_kelamin : $jenis_kelamin<br>";
+echo "umur          : $umur tahun<br>";
+echo "berat badan   : $berat kg<br>";
+echo "tinggi badan  : $tinggi cm<br>";
+echo "menikah       : $menikah";
+?>
